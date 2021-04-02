29504c0b31f214d4b1ce8dd929cbe64e9267e408
Vincent Maximus <vjp.maximus@gmail.com>
2021-04-02 13:58:04 +0200

    added sound

---
+++ assets/js/audio.js
@@ -0,0 +1,12 @@
+let audio = $("#audio-in")[0];
+$(".myName").mouseenter(function() {
+    audio.play();
+});
+
+$("a").mouseenter(function() {
+    audio.play();
+});
+
+$("buttom").mouseenter(function() {
+    audio.play();
+});
